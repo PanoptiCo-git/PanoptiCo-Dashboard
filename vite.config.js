72fa98ed39731fbdc0add216aa99c400d00c8581
sungjun4403 <geulligu89@naver.com>
2026-02-22 02:30:12 +0900

    .

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 // Fixed 2026-02-21: Use different base paths for development and production environments
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/PanoptiCo-Dashboard/' : '/',
+export default defineConfig(({ command, mode }) => ({
+  base: command === 'build' ? '/PanoptiCo-Dashboard/' : '/',
   plugins: [vue()],
   server: {
     port: 5173
