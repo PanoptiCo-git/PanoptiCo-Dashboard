e58227793cdaa9ea07246258965f303088bea2a2
sungjun4403 <geulligu89@naver.com>
2026-02-21 13:57:26 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/PanoptiCo-Dashboard/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/PanoptiCo-Dashboard/' : '/',
   plugins: [vue()],
   server: {
     port: 5173
   }
-})
+}))
